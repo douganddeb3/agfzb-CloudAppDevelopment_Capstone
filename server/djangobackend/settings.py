68ba9ea78867ec7913c10cc09170616d8703e02d
DN <dougn659@gmail.com>
2022-07-21 11:45:01 +0000

    deploy attempt

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", 
+                 "dd3.mybluemix.net",
+                 "dd3.mybluemix.net/static",
+]
 
 
 # Application definition
