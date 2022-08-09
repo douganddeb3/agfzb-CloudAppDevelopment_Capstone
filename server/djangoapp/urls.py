71dc1b91adba1a85840b1e7a620597f70c001e53
DN <dougn659@gmail.com>
2022-08-09 16:04:28 +0000

    get dealers

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
+    path(route='', view=views.get_dealerships, name='index')
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # path for about view
 
     # path for contact us view
