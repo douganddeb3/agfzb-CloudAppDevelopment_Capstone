d916c2ad3967f276a8c60754255b4dd6e7ffadfd
DN <dougn659@gmail.com>
2022-08-12 11:57:31 +0000

    start get reviews

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,9 +10,15 @@ from requests.auth import HTTPBasicAuth
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
     try:
+        if kwargs['st']:
         # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs['st'])
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                        params=kwargs['st'])
+        elif kwargs['dealerId']:
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                        params=kwargs['dealerId']) 
+        else:
+             response = requests.get(url, headers={'Content-Type': 'application/json'},)          
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -26,9 +32,27 @@ def get_request(url, **kwargs):
 
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
+def get_dealer_reviews_from_cf(url, dealer_id):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
+    results=[]
+    json_result = get_request(url, dealerId=dealer_id)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["body"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],state=dealer_doc["state"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
 def get_dealers_from_cf(url, **kwargs):
     results = [] 
     # Call get_request with a URL parameter
